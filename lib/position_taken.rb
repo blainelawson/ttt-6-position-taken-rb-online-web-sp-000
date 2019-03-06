b6b51d4106a743f13faeb848eb78442990288433
Blaine Lawson <blaine.lawson@gmail.com>
2019-03-06 20:15:36 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,3 +1,8 @@
 # code your #position_taken? method here!
-def poisition_taken?
+def poisition_taken?(board, input)
+  if board[input] == "" || board[input] == " "
+    false
+  else
+    true
+  end
 end
